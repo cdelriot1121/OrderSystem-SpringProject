72bce945f1c54145162a6354f34674f1b37e1cde
cdelriot1121 <carandresdelrio1121@gmail.com>
2026-01-06 21:15:14 -0500

    feat: creación y modificación de CustomerService

--- src/main/java/com/example/OrderSystem/service/CustomerService.java
+++ src/main/java/com/example/OrderSystem/service/CustomerService.java
@@ -1,4 +1,34 @@
 package com.example.OrderSystem.service;
 
+
+import com.example.OrderSystem.entities.Customer;
+import com.example.OrderSystem.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+
+    public List<Customer> obtenerTodosLosClientes (){
+        return customerRepository.findAll();
+    }
+
+    public Customer obtenerClientePorEmail (String email) {
+        return customerRepository.findByEmail(email).orElseThrow( () -> new RuntimeException("No se encontro ningun cliente con este email: " + email));
+    }
+
+
+    public Customer guardarCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+
 }
